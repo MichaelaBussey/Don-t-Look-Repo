b212d98a7e16772f1aec1178fae61180bbf0984a
Jodi Chapson <2152594@students.wits.ac.za>
2021-07-27 20:12:58 +0200

    Player and Main Camera Prefab (+accompanying scripts)

---
+++ Don't Look/Assets/Scripts/CamController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    [Header("References")]
+    public Transform player;
+    public float Lerp;
+    public Vector3 CamPos;
+    public float CamY;
+    public bool CamFollow;
+
+
+    private void Start()
+    {
+        CamPos = transform.position;
+        CamFollow = true;
+
+    }
+
+    private void Update()
+    {
+        if (CamFollow)
+        {
+            CamPos = Vector3.Lerp(CamPos, player.position, Lerp);
+            CamPos = new Vector3(CamPos.x, CamPos.y + CamY, -15);
+            transform.position = CamPos;
+        }
+
+    }
+}
---
+++ Don't Look/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //this is one of the movement scripts recommended by Brackeys
+    //it uses the axes for detecting input
+    // also makes use of a rigidbody2d with gravity scale set to 0
+
+    [Header("References")]
+    Vector2 movement;
+
+    Rigidbody2D rb;
+    public float movespeed;
+    public float percentMod; //between 0 and 1
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical") * percentMod;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movespeed * Time.deltaTime);
+    }
+}
