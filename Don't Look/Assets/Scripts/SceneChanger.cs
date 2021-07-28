b09be059503791e682f7d47d2d3080b76e345f5e
Jodi Chapson <2152594@students.wits.ac.za>
2021-07-28 20:55:58 +0200

    Scene Changer Script installed

---
+++ Don't Look/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    
+    //script for changing/reloading scenes, and toggling objects on and off, as well as exiting the game
+
+    public void Scenechanger(int level)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(level);
+    }
+
+    public void Exit()
+    {
+        
+
+        Application.Quit();
+
+
+    }
+
+    public void ToggleObjectOff(GameObject target)
+    {
+
+        //set inactive
+        target.SetActive(false);
+
+    }
+
+    public void ToggleObjectOn(GameObject target)
+    {
+        //set active
+        target.SetActive(true);
+    }
+
+
+    
+    
+
+}
